a517237d571490ee2bc2a0fa945112b83421f2c0
Yasser Akchayat <akchayaty333@gmail.com>
2024-06-06 12:10:00 +0100

    1312

---
+++ precache-manifest.b7cfc62df7de02707db73d33313d6bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bcce4f09cd4626d310bb8e5d216dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b9f6a6fbb548529f6b",
+    "url": "/static/css/main.d01734dc.chunk.css"
+  },
+  {
+    "revision": "e953909b1714aea639da",
+    "url": "/static/js/2.9069b3e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9069b3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b9f6a6fbb548529f6b",
+    "url": "/static/js/main.5476a93d.chunk.js"
+  },
+  {
+    "revision": "fe8e464e820a1c972c1f",
+    "url": "/static/js/runtime-main.15c03aa2.js"
+  }
+]);
